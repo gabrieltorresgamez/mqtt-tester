2762bdac2e33cd875c5f9498694279dd040a0610
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-04-21 10:56:56 +0200

    setup login mask and variable storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,10 @@ import { defineStore } from 'pinia';
 
 export const useStore = defineStore('store', {
 	state: () => ({
-		count: 0,
+		login: "",
+		password: "",
+		topic: "",
+		url: "",
+		configured: false
 	}),
 });
